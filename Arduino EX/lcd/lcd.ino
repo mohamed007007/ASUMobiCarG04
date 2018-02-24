e2a51024977b2ad42b7e9b00fa3001efae4f1fc8
mohamed007007 <36300331+mohamed007007@users.noreply.github.com>
2018-02-24 21:25:54 +0200

    arduino interface with lcd
    interface arduino with lcd

---
+++ Arduino EX/lcd/lcd.ino
@@ -0,0 +1,39 @@
+/*
+ 
+ 
+  The circuit:
+ * LCD RS pin to digital pin 12
+ * LCD Enable pin to digital pin 11
+ * LCD R/W pin to Ground 
+ * LCD VO pin (pin 3) to PWM pin 9
+ * LCD D4 pin to digital pin 5
+ * LCD D5 pin to digital pin 4
+ * LCD D6 pin to digital pin 3
+ * LCD D7 pin to digital pin 2
+
+ */
+
+// include the library code:
+#include <LiquidCrystal.h>
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() {
+  // declare pin 9 to be an output:
+  pinMode(9, OUTPUT);  
+  analogWrite(9, 50);   
+  // set up the LCD's number of columns and rows: 
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("ASUMobiCarG04");
+}
+
+void loop() {
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  lcd.print (millis()/1000);
+}
+
