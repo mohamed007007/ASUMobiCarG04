456cfbfd60ba22f08bc0cace9748f4d8d1bfb7d7
mohamed96hosny <mohamed.mahmoud39623@gmail.com>
2018-04-15 16:44:12 +0200

    Sound sensor code

---
+++ car circuits simulation files/sound_sensor code/sound_sensor.ino
@@ -0,0 +1,33 @@
+int soundSensor=2;
+int LED=4;
+boolean LEDStatus=false;
+
+void setup() {
+ pinMode(soundSensor,INPUT);
+ pinMode(LED,OUTPUT);
+
+}
+
+void loop() {
+
+  int SensorData=digitalRead(soundSensor); 
+  if(SensorData==1){
+
+    if(LEDStatus==false){
+        LEDStatus=true;
+        digitalWrite(LED,HIGH);
+    }
+    else{
+        LEDStatus=false;
+        digitalWrite(LED,LOW);
+    }
+  }
+ } void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
